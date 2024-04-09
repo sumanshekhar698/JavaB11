41635d5d20d7a30385ebdd2fec2ba950c67f1988
Suman Shekhar <suman.sumanshekhar698@gmail.com>
2024-04-09 08:46:37 +0530

    added code

--- src/com/java/oops/encapsulation/Main.java
+++ src/com/java/oops/encapsulation/Main.java
@@ -7,23 +7,27 @@ class Main {
 		Engine eng = new Engine(5);
 		System.out.println(eng.hashCode());
 		Car c1 = new Car(321, "Tata Harrier", eng);
-		String sayMyCar = c1.sayMyCar();
-		System.out.println(sayMyCar);
+		System.out.println(c1.sayMyCar());
 		
 
 
-//		c1.vehichileNo = -9883;
-//		System.out.println(c1.getVehichileNo());
-//
+//		c1.vehichileNo = -9883;//private  things cannot be accessed directly outside their class
+		System.out.println(c1.getVehichileNo());//however we can have indirect access
+
 //		int vehichileNo = c1.getVehichileNo();
 //		vehichileNo = 999999;
-//
+		
+//		String sayMyCar = c1.sayMyCar();
+		System.out.println(c1.sayMyCar());
+		
+		
 		Engine eng1 = c1.getEng();
 		System.out.println(eng1.hashCode());
 		eng1.engineNumber = -998;
 //		
 //		System.out.println(c1.getVehichileNo());
-		System.out.println(sayMyCar);
+//		System.out.println(sayMyCar);
+//		System.out.println(c1.sayMyCar());
 
 	}
 
